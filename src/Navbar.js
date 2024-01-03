dad3b0d3d3251059f35bc65984366d1eed3fe133
Eda Albert <Albert@edas-mbp.home>
2024-01-03 18:59:58 -0500

    add NavBar -component

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+        <a href="/home">Home</a>
+        <a href="/NewBlog">New Blog</a>
+    </div>
+  )
+}
+
+export default Navbar
