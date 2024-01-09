406ad2544897b3e1236d8439877b0e87b5b16a8f
Albert Eda <alberteda32@gmail.com>
2024-01-09 13:44:28 -0500

    add Blogs component

---
+++ src/Blogs.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Blogs = ({blogs, handleDelete}) => {
+  return (
+    <div className="blog-preview-container">
+        {
+        blogs.map((blog) => (
+            <div className="blog-preview">
+                <h1>{blog.title}</h1>
+                <p className="blog-author">Written By {blog.author}</p>
+                <button 
+                    className="blog-delete-bttn"
+                    onClick={() => (
+                        handleDelete(blog.id)
+                    )}
+                >
+                    delete blog
+                </button>
+            </div>
+        ))
+    }
+    </div>
+  )
+}
+
+export default Blogs
