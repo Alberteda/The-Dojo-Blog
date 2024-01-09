fcc1efa96dca83b83f406f98819aef33013c6c7e
Albert Eda <alberteda32@gmail.com>
2024-01-08 23:09:52 -0500

    remove museum -hook

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,19 +1,11 @@
 import React from 'react'
-import { useMetMuseumApi } from './api-hooks/useMetMuseumApi'
 
 const HomePage = () => {
 
-    const {artPieces} = useMetMuseumApi()
 
   return (
     <div className="home-page">
         <h1>Home Page</h1>
-
-        <div className="art-pieces">
-            {artPieces.map((objectID) => (
-                <div key={objectID}>{console.log('ID==>',objectID)}</div>
-            ))}
-        </div>
     </div>
   )
 }
