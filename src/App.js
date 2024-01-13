952ad96ec1425401adfb5f4d67043121cf0ec6df
Albert Eda <alberteda32@gmail.com>
2024-01-13 11:43:45 -0500

    add CreateBlog component and Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import Navbar from './Navbar';
-import HomePage from './HomePage';
+import Navbar from "./Navbar";
+import HomePage from "./HomePage";
+import CreateBlog from "./CreateBlog";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className='app-container'>
-      <Navbar/>
-      <HomePage />
-    </div>
-      
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/create">
+              <CreateBlog />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
