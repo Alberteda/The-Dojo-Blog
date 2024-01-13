f1a335f73a7afdb7ae4299e3c9ac7ea2fe1e407d
Albert Eda <alberteda32@gmail.com>
2024-01-13 13:58:29 -0500

    update API call, add elements for blog title and body

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useBlogApi } from "./api-hooks/useBlogApi";
 
 const BlogDetails = () => {
 
     const { id } = useParams()
+    const { blogs } = useBlogApi(`http://localhost:8000/blogs/${id}`)
 
     return (
-    <div className="blog-details">
-        <h2>Blog Details - {id}</h2>
-    </div>
+        <div className="blog-details">
+            <h2>Blog Details</h2>
+            <h3>{blogs?.title}</h3>
+            <h3>{blogs?.body}</h3>
+        </div>
     );
 };
 
